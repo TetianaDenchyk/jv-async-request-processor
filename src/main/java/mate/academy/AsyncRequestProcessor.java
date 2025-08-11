6225cbca2270da146ab37ce504d83a0810471514
tetiana <tetiana.denchyk@ocado.com>
2025-08-11 11:25:12 +0300

    completed task

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,35 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private static Map<String, UserData> cache = new java.util.concurrent.ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            UserData foundUser = cache.get(userId);
+            if (foundUser != null) {
+                return foundUser;
+            }
+            UserData newUser = new UserData(userId, "Details for " + userId);
+            storeInCache(userId, newUser);
+            return newUser;
+        }, executor);
+    }
+
+    private void storeInCache(String userId, UserData userData) {
+        executor.execute(() -> cache.put(userId, userData));
     }
 }
